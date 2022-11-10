b8f87992ed6535a86d065f1ea70284926e84aea6
Nicolas Huber <nicolas.huber@jkweb.ch>
2022-11-10 11:54:34 +0100

    basic data structure

---
+++ todo_app/migrations/Version20221110105135.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221110105135 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE todo (id INT AUTO_INCREMENT NOT NULL, belongs_to_id INT DEFAULT NULL, name VARCHAR(255) NOT NULL, description VARCHAR(255) DEFAULT NULL, important TINYINT(1) DEFAULT NULL, due_to DATE DEFAULT NULL, INDEX IDX_5A0EB6A033C857F5 (belongs_to_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(180) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE todo ADD CONSTRAINT FK_5A0EB6A033C857F5 FOREIGN KEY (belongs_to_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE todo DROP FOREIGN KEY FK_5A0EB6A033C857F5');
+        $this->addSql('DROP TABLE todo');
+        $this->addSql('DROP TABLE user');
+    }
+}
---
+++ todo_app/src/Entity/Todo.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TodoRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TodoRepository::class)
+ */
+class Todo
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $important;
+
+    /**
+     * @ORM\Column(type="date", nullable=true)
+     */
+    private $due_to;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="todos")
+     */
+    private $belongs_to;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function isImportant(): ?bool
+    {
+        return $this->important;
+    }
+
+    public function setImportant(?bool $important): self
+    {
+        $this->important = $important;
+
+        return $this;
+    }
+
+    public function getDueTo(): ?\DateTimeInterface
+    {
+        return $this->due_to;
+    }
+
+    public function setDueTo(?\DateTimeInterface $due_to): self
+    {
+        $this->due_to = $due_to;
+
+        return $this;
+    }
+
+    public function getBelongsTo(): ?User
+    {
+        return $this->belongs_to;
+    }
+
+    public function setBelongsTo(?User $belongs_to): self
+    {
+        $this->belongs_to = $belongs_to;
+
+        return $this;
+    }
+}
