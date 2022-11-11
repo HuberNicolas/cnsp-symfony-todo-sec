c33aa717ff47590fd12ad4ec9b88218365d3b64b
Nicolas Huber <nicolas.huber@jkweb.ch>
2022-11-11 11:10:22 +0100

    todo form

--- todo_app/src/Controller/TodoController.php
+++ todo_app/src/Controller/TodoController.php
@@ -2,14 +2,18 @@
 
 namespace App\Controller;
 
+use App\Entity\Todo;
+use App\Form\TodoType;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TodoController extends AbstractController
 {
     /**
-     * @Route("/todo", name="app_todo")
+     * @Route("/todo", name="todo_index")
      */
     public function index(): Response
     {
@@ -17,4 +21,33 @@ class TodoController extends AbstractController
             'controller_name' => 'TodoController',
         ]);
     }
+
+    /**
+     * @Route("/todo/new", name="todo_new")
+     */
+    public function new(ManagerRegistry $doctrine, Request $request): Response
+    {
+        $entityManager = $doctrine->getManager();
+        $todo = new Todo();
+
+
+        $form = $this->createForm(TodoType::class, $todo);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $todo = $form->getData();
+
+            $entityManager->persist($todo);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('todo_index');
+        }
+
+        return $this->render('todo/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+    }
+
 }
--- todo_app/src/Entity/User.php
+++ todo_app/src/Entity/User.php
@@ -165,4 +165,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
         return $this;
     }
+
+    public function __toString()
+    {
+        return $this->getEmail();
+    }
 }
